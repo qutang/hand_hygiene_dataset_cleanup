4c1e8d1a6caaf1294258fec6b6e3daed96847183
qutang <qu.tang@outlook.com>
2020-11-09 21:24:43 -0500

    feat: Support ALL participants

--- run_post_clean.py
+++ run_post_clean.py
@@ -3,13 +3,21 @@ import post_clean_up as post_clean
 from loguru import logger
 import os
 import sys
+import arus
 
 if __name__ == "__main__":
     # configurations
-    pids = sys.argv[1].split(',')
+    print(sys.argv)
     root = os.path.expanduser("D:/Datasets/hand_hygiene_dataset")
+    if sys.argv[1] == 'ALL':
+        pids = arus.mh.get_pids(root)
+        pids = list(filter(lambda pid: pid.startswith('P'), pids))
+    else:
+        pids = sys.argv[1].split(',')
 
+    print(pids)
     for pid in pids:
+        logger.info(f"Post clean {pid}")
         handle_id = logger.add(os.path.join(
             root, pid, "Logs", "post_clean.log"))
         post_clean.convert_to_mhealth(root, pid)
