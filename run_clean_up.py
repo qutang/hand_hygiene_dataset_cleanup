b3c2b351272251d597516a78fd6874acf9508e76
qutang <qu.tang@outlook.com>
2020-11-10 03:04:52 -0500

    feat: Support splitted sessions

--- run_clean_up.py
+++ run_clean_up.py
@@ -10,14 +10,47 @@ if __name__ == "__main__":
     root = os.path.expanduser("D:/Datasets/hand_hygiene_dataset")
     sr = 80
     date_range = None
-    auto_range = "W-WED"
+    auto_range = "W-TUE"
     skip_sync = False
     correct_ori = True
 
+    profile_names = {
+        'P6': ['Wonderwoman', 'Demo'],
+        'P2': ['Black Widow', 'Demo']
+    }
+
+    date_ranges = {
+        'P6_1': [['2020-08-06', '2020-08-07'], ['2020-11-08', '2020-11-09']],
+        'P6_2': [['2020-08-07', '2020-08-08'], ['2020-11-08', '2020-11-09']],
+        'P6_3': [['2020-08-07', '2020-08-08'], ['2020-11-09-19', '2020-11-09-20']],
+        'P6_4': [['2020-08-07', '2020-08-08'], ['2020-11-09-19', '2020-11-09-20']],
+        'P6_5': [['2020-08-08', '2020-08-09'], ['2020-11-09-20', '2020-11-09-21']],
+        'P6_6': [['2020-08-08', '2020-08-09'], ['2020-11-09-20', '2020-11-09-21']]
+    }
+
     for pid in pids:
         handle_id = logger.add(os.path.join(root, pid, "Logs", "cleanup.log"))
-        clean_up.convert_to_mhealth(
-            root, pid, skip_sync, correct_orientation=correct_ori, remove_exists=True)
-        arus.cli.convert_to_signaligner_both(
-            root, pid, sr, date_range, auto_range)
+        if pid.split('_')[0] in ['P2', 'P6']:
+            # clean_up.convert_to_mhealth(
+            #     root, pid, skip_sync, correct_orientation=correct_ori,
+            #     annot_profile=profile_names[pid.split('_')[0]][0],
+            #     raw_location='subj_folder',
+            #     remove_exists=True)
+            clean_up.convert_to_mhealth(
+                root, pid, skip_sync, correct_orientation=correct_ori,
+                annot_profile=profile_names[pid.split('_')[0]][1],
+                raw_location='cross_folder',
+                remove_exists=False)
+            # arus.cli.convert_to_signaligner_both(
+            #     root, pid, sr, date_range=date_ranges[pid][0], auto_range=auto_range)
+            arus.cli.convert_to_signaligner_both(
+                root, pid, sr, date_range=date_ranges[pid][1], auto_range=auto_range)
+        else:
+            clean_up.convert_to_mhealth(
+                root, pid, skip_sync, correct_orientation=correct_ori,
+                annot_profile='**',
+                raw_location='subj_folder',
+                remove_exists=True)
+            arus.cli.convert_to_signaligner_both(
+                root, pid, sr, date_range=None, auto_range=auto_range)
         logger.remove(handle_id)
